3a73a77ec82a19e4f199a54160edfb43e801aac8
lizheng <v-lizheng@expediagroup.com>
2022-07-02 20:25:44 +0800

    add backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+
+
+const app = new Koa();
+
+// 2.绑定中间件
+// 绑定第一层中间件
+const respDurationMiddleware = require('./middleware/koa_response_duration')
+app.use(respDurationMiddleware)
+    // 绑定第二层中间件
+const respHeaderMiddleware = require('./middleware/koa_response_header')
+app.use(respHeaderMiddleware)
+    // 绑定第三层中间件
+const respDataMiddleware = require('./middleware/koa_response_data')
+app.use(respDataMiddleware)
+
+app.listen(8080)
